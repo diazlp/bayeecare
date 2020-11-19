a6e796ed8741e512b8cb9d2efaaa9537edcc839b
diazlp <diazlinggaputra@gmail.com>
2020-11-19 07:18:06 +0700

    under heavy construction

--- small-script.js
+++ small-script.js
@@ -21,9 +21,9 @@ document.body.style.backgroundColor = "White";
 
 // slider script is as follows:
 var images = [
-  'https://www.oeteo.co/wp/wp-content/uploads/2020/11/11.11SALE_Website-Banner.jpg',
-  'https://cdn.shopify.com/s/files/1/0655/7411/files/Maternity_Nursing_Bra-3_ee87fba8-d297-4e13-9f22-7528460107ff_1060x.png?v=1541141387',
-  'https://cdn.singpromos.com/wp-content/uploads/2020/10/Infantree-Baby-Warehouse-f-1-Oct-2020.jpg'
+  'image-src/img1.jpg',
+  'image-src/img2.jpg',
+  'image-src/img3.jpg'
 ];
 
 var num = 0;
